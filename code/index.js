5e91b2415c7d3cff1515c56d0b65b035b7d6e8be
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-01 15:48:35 +0500

    update

--- code/index.js
+++ code/index.js
@@ -37,6 +37,9 @@ const preloader = document.querySelector('.c-car-spinner');
 const send = document.querySelector('.send');
 const form = document.querySelector('#partner-form');
 const bottomSheet = document.querySelector('.bottomsheet');
+const carBrandsSelect = document.getElementById("car-brands-select");
+const carModelsSelect = document.getElementById("car-models-select");
+const selectElement = document.getElementById('field_select-type');
 
 function showBottomBar(text) {
     document.querySelector('.bottom-sheet').style.display = 'block';
@@ -87,9 +90,6 @@ async function getCarBrandsAndModels() {
         console.error(err);
     }
 
-    const carBrandsSelect = document.getElementById("car-brands-select");
-    const carModelsSelect = document.getElementById("car-models-select");
-
     for (let row of car_values) {
         const brand = row[0];
         const option = document.createElement("option");
@@ -119,8 +119,6 @@ async function getCarBrandsAndModels() {
 }
 
 async function fetchData() {
-
-    const selectElement = document.getElementById('field_select-type');
 
     try {
         const response = await fetch('/getdata');
@@ -253,52 +251,90 @@ document.addEventListener("click", function (event) {
     });
 })
 
-let multiselects = document.querySelectorAll(".multiselect_block");
+/**
+ * Инициализация переменных для элементов мультиселекта.
+ */
+let multiselect = document.querySelector(".multiselect_block");
+let label = multiselect.querySelector(".field_multiselect");
+let select = multiselect.querySelector(".field_select");
+let text = label.innerHTML;
 
-multiselects.forEach(multiselect => {
-    let label = multiselect.querySelector(".field_multiselect");
-    let select = multiselect.querySelector(".field_select");
-    let text = label.innerHTML;
+/**
+ * Обработчик события клика вне выпадающего списка для скрытия списка.
+ */
+document.addEventListener("click", function (event) {
+    // Проверяем, содержит ли событие элемент выпадающего списка или его метки 
+    if (!select.contains(event.target) && !label.contains(event.target)) {
+        // Если событие не происходит внутри списка или его метки, скрываем список
+        select.style.display = "none";
+    }
+});
 
-    label.addEventListener("click", () => {
-        select.style.display = select.style.display === "block" ? "none" : "block";
+/**
+ * Функция создания кнопок для выбранных опций мультиселекта.
+ * @param {HTMLOptionElement} option - Выбранная опция.
+ * @param {HTMLElement} label - Элемент для отображения выбранных значений.
+ * @param {HTMLSelectElement} select - Элемент мультиселекта.
+ * @param {string} text - Текст для отображения в мультиселекте.
+ */
+function do_buttons(option, label, select, text) {
+    let button = document.createElement("button");
+    button.type = "button";
+    button.className = "btn_multiselect";
+    button.textContent = option.value;
 
-        if (select.style.display === "block") {
-            select.focus();
-        }
-    });
+    button.onclick = event => {
+        event.stopPropagation(); // Останавливаем распространение события
+        option.selected = false;
+        button.remove();
+        if (!select.selectedOptions.length) label.innerHTML = text;
+    };
+    label.append(button);
+}
 
-    select.addEventListener("change", () => {
-        let selectedOptions = select.selectedOptions;
-        label.innerHTML = "";
-        for (let option of selectedOptions) {
-            let button = document.createElement("button");
-            button.type = "button";
-            button.className = "btn_multiselect";
-            button.textContent = option.value;
+/**
+ * Обработчик события клика по метке для отображения или скрытия выпадающего списка.
+ */
+label.addEventListener("click", () => {
+    select.style.display = select.style.display === "block" ? "none" : "block";
 
-            if (!chat_id) {
-                button.style.backgroundColor = colors.button_bg;
-                button.style.color = colors.button_text;
-            }
+    if (select.style.display === "block") {
+        select.focus();
+    }
+});
 
-            button.onclick = event => {
-                event.stopPropagation(); // Останавливаем распространение события
-                option.selected = false;
-                button.remove();
-                if (!select.selectedOptions.length) label.innerHTML = text;
-            };
-            label.append(button);
-        }
-    });
+/**
+ * Обработчик изменения выбранных опций в мультиселекте для отображения кнопок выбора.
+ */
+select.addEventListener("change", () => {
+    let selectedOptions = select.selectedOptions;
+    label.innerHTML = "";
+    for (let option of selectedOptions) {
+        do_buttons(option, label, select, text);
+    }
+});
 
-    // Добавляем обработчик событий для кнопок внутри мультиселекта, чтобы предотвратить открытие опций
-    multiselect.querySelectorAll(".btn_multiselect").forEach(button => {
-        button.addEventListener("click", event => {
-            event.stopPropagation(); // Останавливаем распространение события
-        });
+// Добавляем обработчик событий для кнопок в мультиселекте для предотвращения открытия опций.
+multiselect.querySelectorAll(".btn_multiselect").forEach(button => {
+    button.addEventListener("click", event => {
+        event.stopPropagation(); // Останавливаем распространение события
     });
 });
+
+/**
+ * Асинхронная функция select_all для выбора всех опций в мультиселекте.
+ */
+async function select_all() {
+    let options = select.options;
+    label.innerHTML = "";
+
+    for (let i = 0; i < options.length; i++) {
+        const option = options[i];
+        option.selected = true;
+
+        do_buttons(option, label, select, text);
+    }
+}
 
 
 if (chat_id) {
@@ -452,6 +488,7 @@ async function preload() {
 
     await fetchData();
     await getCarBrandsAndModels();
+    await select_all();
 
     preloader.style.display = "none";
     container.style.display = "flex";
