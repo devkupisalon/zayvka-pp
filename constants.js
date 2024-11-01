6fd337289a551e2102dd3f2d4807948fc938aa8d
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-11-01 09:55:57 +0500

    update

--- constants.js
+++ constants.js
@@ -19,7 +19,6 @@ const constants = {
     come_status: 'Ждем клиента'
 };
 
-// Чтение JSON-файла
 fs.readFile('managers.json', 'utf8', (err, data) => {
   if (err) {
       console.error(err);
@@ -28,33 +27,5 @@ fs.readFile('managers.json', 'utf8', (err, data) => {
   const jsonData = JSON.parse(data);
   managers_map = jsonData;
 });
-
-// const managers_map = {
-//     '8fb4889c-3f58-4cec-a11f-9bd446049914':
-//     {
-//       m: 'Алексей',
-//       link: 'https://t.me/kupisalon_zayvka_bot/start?startapp=8fb4889c-3f58-4cec-a11f-9bd446049914'
-//     },
-//     '9ab266c0-867a-4017-980e-76460fd47630':
-//     {
-//       m: 'Ирина',
-//       link: 'https://t.me/kupisalon_zayvka_bot/start?startapp=9ab266c0-867a-4017-980e-76460fd47630'
-//     },
-//     '6e9f227b-3054-465e-a81c-ee660aa0a90a':
-//     {
-//       m: 'Николоз',
-//       link: 'https://t.me/kupisalon_zayvka_bot/start?startapp=6e9f227b-3054-465e-a81c-ee660aa0a90a'
-//     },
-//     'c27c32f5-34de-48ea-a711-cbf8da0c080c':
-//     {
-//       m: 'Дмитрий',
-//       link: 'https://t.me/kupisalon_zayvka_bot/start?startapp=c27c32f5-34de-48ea-a711-cbf8da0c080c'
-//     },
-//     '482634aa-1137-478b-a749-4f49fb884d77':
-//     {
-//       m: 'Андрей',
-//       link: 'https://t.me/kupisalon_zayvka_bot/start?startapp=482634aa-1137-478b-a749-4f49fb884d77'
-//     }
-//   };
 
 export { constants, __dirname, managers_map };
